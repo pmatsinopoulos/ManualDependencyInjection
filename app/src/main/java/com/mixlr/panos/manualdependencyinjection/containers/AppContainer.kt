c3eabdcef76c8bc31cfc4b1ebc75cf425881e871
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-06-20 11:41:21 +0300

    Add an AppContainer that will hold all the dependencies instantiated

---
+++ app/src/main/java/com/mixlr/panos/manualdependencyinjection/containers/AppContainer.kt
@@ -0,0 +1,20 @@
+package com.mixlr.panos.manualdependencyinjection.containers
+
+import com.mixlr.panos.manualdependencyinjection.datasources.UserLocalDataSource
+import com.mixlr.panos.manualdependencyinjection.datasources.UserRemoteDataSource
+import com.mixlr.panos.manualdependencyinjection.repositories.UserRepository
+import com.mixlr.panos.manualdependencyinjection.retrofitservices.LoginRetrofitService
+
+// Container of objects shared across the whole app
+class AppContainer {
+
+    // Since you want to expose userRepository out of the container, you need to satisfy
+    // its dependencies as you did before
+    private val retrofit = LoginRetrofitService()
+
+    private val remoteDataSource = UserRemoteDataSource(retrofit)
+    private val localDataSource = UserLocalDataSource()
+
+    // userRepository is not private; it'll be exposed
+    val userRepository = UserRepository(localDataSource, remoteDataSource)
+}
